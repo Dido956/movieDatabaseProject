cc4625f191b13e9e5a052e48f846f0e8d40d5a8f
Dido956 <dido956@gmail.com>
2023-11-11 20:15:22 +0200

    Added movieNotFound page

--- src/main/java/bg/softuni/moviedatabase/controller/HomePageController.java
+++ src/main/java/bg/softuni/moviedatabase/controller/HomePageController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -31,6 +30,6 @@ public class HomePageController {
             return "redirect:/movies/details/" + movie.getId();
         }
         //TODO: redirect somewhere on movieNotFound();
-        return "/index";
+        return "/movieNotFound";
     }
 }
